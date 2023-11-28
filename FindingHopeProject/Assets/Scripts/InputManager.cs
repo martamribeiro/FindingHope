598dcb3cb4d53f9c6663f50374febd5281296b2b
Cristóvão Sampaio <cristovaogmsampaio0@gmail.com>
2023-11-28 18:19:40 +0000

    Added basic Animations for the player movement.

--- FindingHopeProject/Assets/Scripts/InputManager.cs
+++ FindingHopeProject/Assets/Scripts/InputManager.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
+    AnimatorManager animatorManager;
 
     public Vector2 movementInput;
+    private float moveAmount;
     public float verticalInput;
     public float horizontalInput;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
 
     private void OnEnable()
     {
@@ -37,5 +44,8 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
     }
 }
