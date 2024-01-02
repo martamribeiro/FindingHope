0e9bb51aa7e4eb32c2efcd151a88642756370140
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2024-01-02 14:04:29 +0000

    Updated talking key to F, for interaction consistency between universes

--- FindingHopeProject/Assets/AIUniverse/AIUniverseTravelToMain.cs
+++ FindingHopeProject/Assets/AIUniverse/AIUniverseTravelToMain.cs
@@ -5,22 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class AIUniverseTravelToMain : MonoBehaviour
 {
-    public GameObject triggerObject;  // Reference to the object to check activation status
 
     void Update()
     {
-        // Check if the player is inside the trigger area and pressing the "T" key
-        if (Input.GetKeyDown(KeyCode.T) && IsTriggerObjectActive())
+        // Check if the player is pressing the "T" key
+        if (Input.GetKeyDown(KeyCode.T))
         {
             // Go to universe 1
             SceneManager.LoadScene("MainUniverse");
         }
-    }
-
-    bool IsTriggerObjectActive()
-    {
-        // Check if the trigger object is active
-        return triggerObject.activeSelf;
     }
 
 }
--- FindingHopeProject/Assets/AIUniverse/Convai/Scripts/ConvaiNPC.cs
+++ FindingHopeProject/Assets/AIUniverse/Convai/Scripts/ConvaiNPC.cs
@@ -206,14 +206,14 @@ namespace Convai.Scripts
         /// </summary>
         private void HandleCharacterActions()
         {
-            // Start recording audio when the T key is pressed
-            if (Input.GetKeyDown(KeyCode.T))
+            // Start recording audio when the F key is pressed
+            if (Input.GetKeyDown(KeyCode.F))
             {
                 UpdateActionConfig();
                 StartListening();
             }
-            // Stop recording audio when the T key is released
-            else if (Input.GetKeyUp(KeyCode.T))
+            // Stop recording audio when the F key is released
+            else if (Input.GetKeyUp(KeyCode.F))
             {
                 StopListening();
             }
