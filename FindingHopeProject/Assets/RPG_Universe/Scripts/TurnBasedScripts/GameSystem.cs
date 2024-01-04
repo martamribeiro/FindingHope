46d1743dd551059706e57c1a2066f26bf94764b7
Cristóvão Sampaio <cristovaogmsampaio0@gmail.com>
2024-01-04 00:16:49 +0000

    RPG Universe added.

--- FindingHopeProject/Assets/RPG_Universe/Scripts/TurnBasedScripts/GameSystem.cs
+++ FindingHopeProject/Assets/RPG_Universe/Scripts/TurnBasedScripts/GameSystem.cs
@@ -1,8 +1,5 @@
 using JetBrains.Annotations;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
